548b62cf57b04bf16cdc9f92a156e66f4b97ebf0
alecasentini <ale.casentini@gmail.com>
2023-03-27 15:41:26 +0200

    Aggiunto mail con stile

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -1,10 +1,15 @@
-let mail = prompt ("Scrivi il tuo indirizzo Email", "mario.rossi@gmail.com")
+function control(){
+    let mail = document.getElementById('emailInput').value;
 
-let validMail = ["mario.rossi@gmail.com", "harry.potter@gmail.com", "tom.cruise@gmail.com", "jeff.bezos@gmail.com", "elon.musk@gmail.com" ]
+    let validMail = ["mario.rossi@gmail.com", "harry.potter@gmail.com", "tom.cruise@gmail.com", "jeff.bezos@gmail.com", "elon.musk@gmail.com" ]
 
-if (validMail.includes(mail)){
-    console.log ("Accesso consentito")
-}
-else{
-    console.log ("Accesso negato")
+    if (validMail.includes(mail)){
+        const element = `<div class="fw-semibold text-success py-3">${"Accesso consentito"}</div>`;
+        result.innerHTML = element;
+    }
+    else{
+        const element = `<div class="fw-semibold text-danger py-3">${"Accesso negato"}</div>`;
+        result.innerHTML = element;
+    }
+
 }
