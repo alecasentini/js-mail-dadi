b713303e023aae3f57c1d071d74d1ac8ee6a71b0
alecasentini <ale.casentini@gmail.com>
2023-03-27 14:35:27 +0200

    Soluzione Dadi su console

---
+++ Dadi/assets/js/main.js
@@ -0,0 +1,15 @@
+let x = Math.floor((Math.random() * 6) + 1);
+console.log(x)
+
+let y = Math.floor((Math.random() * 6) + 1);
+console.log(y)
+
+if (x>y){
+    console.log("hai vinto!")
+}
+else if(x<y){
+    console.log("hai perso!")
+}
+else{
+    console.log("Pareggio")
+}
